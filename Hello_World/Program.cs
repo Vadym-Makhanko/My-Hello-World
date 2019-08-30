e67782fcb203489b3a14f9e87364631b9365bf7c
Vadym Makhanko <vadym-makhanko@outlook.com>
2019-08-30 18:24:42 +0200

    Commit 2 : - Comminting my project

---
+++ Hello_World/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Hello World");
+                Console.WriteLine("\nPlease, press the button 5\n");
+                char chetyre = Console.ReadKey().KeyChar;
+
+                if (chetyre == '5')
+                {
+                    Console.WriteLine("\n\nCorrect");
+                }
+                else
+                {
+                    Console.WriteLine("\n\nIncorrect");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
